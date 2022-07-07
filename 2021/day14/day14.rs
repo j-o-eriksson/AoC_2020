d7c730e7b2bdec7e125445838fe2911e2a0d8cc3
Jon <jonatan.o.eriksson@gmail.com>
2022-07-07 17:56:33 +0200

    day 14 part 1

--- 2021/day14/day14.rs
+++ 2021/day14/day14.rs
@@ -1,17 +1,37 @@
 // AoC 2021 day 13
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn find_insertions(chain: &Vec<char>, rules: &HashMap<&str, char>) -> Vec<(char, usize)> {
     let mut insertions: Vec<(char, usize)> = Vec::new();
     chain.windows(2).enumerate().for_each(|(i, w)| {
         let pair = format!("{}{}", w[0], w[1]);
         match rules.get(pair.as_str()) {
-            Some(c) => insertions.push((*c, i)),
+            Some(c) => insertions.push((*c, i + 1)),
             None => (),
         }
     });
     insertions
+}
+
+fn next_chain(chain: &Vec<char>, insertions: &Vec<(char, usize)>) -> Vec<char> {
+    let mut next_chain = Vec::new();
+
+    let mut i0 = 0;
+    for (c, i1) in insertions {
+        for i in i0..*i1 {
+            next_chain.push(chain[i]);
+        }
+        next_chain.push(*c);
+        i0 = *i1;
+    }
+
+    for i in i0..chain.len() {
+        next_chain.push(chain[i]);
+    }
+
+    next_chain
 }
 
 fn main() {
@@ -23,17 +43,19 @@ fn main() {
             (pair, insert.chars().next().unwrap())
         })
         .collect();
-    println!("{:?}", rules);
 
-    let chain: Vec<char> = a.chars().collect();
+    let mut chain: Vec<char> = a.chars().collect();
 
-    for i in 0..(chain.len() - 1) {
-        let pair: String = format!("{}{}", chain[i], chain[i + 1]);
-        match rules.get(pair.as_str()) {
-            Some(c) => println!("insert {} after {}", c, i),
-            None => (),
-        }
+    for _ in 0..10 {
+        let insertions = find_insertions(&chain, &rules);
+        chain = next_chain(&chain, &insertions);
     }
 
-    println!("{:?}", find_insertions(&chain, &rules));
+    let set: HashSet<&char> = chain.iter().collect();
+    for c in &set {
+        let count = chain.iter().filter(|ci| ci == c).count();
+        println!("{}: {}", c, count);
+    }
+    println!("{:?}", chain.len());
+    println!("{:?}", set);
 }
